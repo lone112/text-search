80cca0b85a615a7601b906977a97ce1814e43c94
fei.xiao <fei.xiao@yungoal.com>
2017-04-20 13:54:16 +0800

    change main activity to kt

---
+++ app/src/main/java/com/fei/textsearch/LinesViewActivity.kt
@@ -0,0 +1,12 @@
+package com.fei.textsearch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LinesViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lines_view)
+    }
+}
